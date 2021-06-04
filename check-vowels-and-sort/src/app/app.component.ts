2c6771c72ef7df8e53b43bad280686b2de930cf1
Sadusha Nadeesh <sadusha1994@gmail.com>
2021-06-04 12:11:05 +0530

    check vowels and sort

--- check-vowels-and-sort/src/app/app.component.ts
+++ check-vowels-and-sort/src/app/app.component.ts
@@ -7,4 +7,33 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'check-vowels-and-sort';
+  isVowels = false;
+  result :string[]= [];
+  text: string[]=[];
+  wordCount: number=0;
+  vowelWords: number=0;
+  
+
+  checkParagraph(value: any) {
+    let containVowel;
+    let uncheckedText = value.paragraph;
+    this.text = uncheckedText.split(' ').sort();
+    console.log(this.text);
+    for (let word of this.text) {
+      containVowel = word.match(/[aeiou]/gi);
+      if (containVowel) {
+        this.result.push(word);
+      }
+    }
+    if(this.result.length>0){
+      this.isVowels = true;
+      this.wordCount = this.text.length;
+      this.vowelWords = this.result.length
+    }
+    
+  }
+
+  reloadCurrentPage() {
+    window.location.reload();
+   }
 }
